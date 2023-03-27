99a3ef6ee9ae7d2db704d726694602906fc38a03
CİHAN ÖZÇELİK <cihan84_ozcelik@hotmail.com>
2023-03-28 02:10:35 +0300

    entry-comments

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Queries/GetUserDetail/GetUserDetailQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using BlazorSozluk.Api.Application.Interfaces.Repositories;
+using BlazorSozluk.Common.Models.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Application.Features.Queries.GetUserDetail
+{
+    public class GetUserDetailQueryHandler : IRequestHandler<GetUserDetailQuery, UserDetailViewModel>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public GetUserDetailQueryHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public Task<UserDetailViewModel> Handle(GetUserDetailQuery request, CancellationToken cancellationToken)
+        {
+           
+        }
+    }
+}
